d69aa56f025ba42a43b2c8640a4c77e531515cfd
Snehashis Gharai <connectsnehashis22@gmail.com>
2025-08-14 15:20:06 +0530

    event page done

---
+++ events.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Events | Zilla Parishad Palghar</title>
+    <?php include_once "include/header_script.php"; ?>
+</head>
+
+<body>
+    <?php include_once "include/header.php"; ?>
+    <img src="https://cdnbbsr.s3waas.gov.in/s34dd9cec1c21bc54eecb53786a2c5fa09/uploads/2024/12/202412091606075104.jpg" alt="Tailwind CSS Logo" class="mx-auto min-h-[115px] object-cover">
+    <div class="bg-[#F6F3EE]">
+        <div class="max-w-7xl min-h-[600px] mx-auto px-4 py-3">
+
+            <!-- Main Title with view changes -->
+            <div class="max-w-7xl mx-auto py-6 flex justify-between items-center">
+                <h1 class="text-[28px] font-semibold text-gray-900">Events</h1>
+                <div class="flex gap-3">
+                    <button>
+                        <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="11.4191" height="11.4191" fill="black" />
+                            <rect x="14.5815" width="11.4191" height="11.4191" fill="black" />
+                            <rect x="14.5815" y="14.5811" width="11.4191" height="11.4191" fill="black" />
+                            <rect y="14.5811" width="11.4191" height="11.4191" fill="black" />
+                        </svg>
+                    </button>
+
+                    <button>
+                        <svg width="26" height="25" viewBox="0 0 26 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="26" height="5" fill="black" />
+                            <rect y="10" width="26" height="5" fill="black" />
+                            <rect y="20" width="26" height="5" fill="black" />
+                        </svg>
+                    </button>
+
+                </div>
+            </div>
+
+            <!-- Event Details -->
+            <div class="w-full border border-gray-300 p-1">
+                <h2 class="p-4">No events found.</h2>
+            </div>
+
+        </div>
+    </div>
+
+    <?php include_once "include/footer.php"; ?>
+    <?php include_once "include/footer_script.php"; ?>
+
+</body>
+
+</html>
